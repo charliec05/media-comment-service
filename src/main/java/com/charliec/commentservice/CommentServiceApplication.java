03bda6d56996f291f18f67071dec1be2c204cff5
charliechen <jchen264@uw.edu>
2026-01-14 22:21:48 -0800

    chore: init spring boot project

---
+++ src/main/java/com/charliec/commentservice/CommentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.charliec.commentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentServiceApplication.class, args);
+	}
+
+}
